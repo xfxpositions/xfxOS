7ebd4e063d9087c4a90e0d4f76b38e784596f916
xfxpositions <xfxpositions@gmail.com>
2023-02-08 00:30:25 +0300

    building script

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
-
-mkdir build
-mkdir build/grub
-
-
-
+cargo build --release --bin kernel --out-dir ./iso/kernel.bin -Z unstable-options
+mkdir -p iso 
+mkdir -p  iso/boot
+mkdir -p  iso/boot/grub
+echo 'set timeout=3' > iso/boot/grub/grub.cfg
+echo 'set default=0' >> iso/boot/grub/grub.cfg
+echo '' >> iso/boot/grub/grub.cfg
+echo 'menuentry "xfxOS"{' >> iso/boot/grub/grub.cfg
+echo '	multiboot /boot/kernel' >> iso/boot/grub/grub.cfg
+echo '  boot' >> iso/boot/grub/grub.cfg
+echo '}' >> iso/boot/grub/grub.cfg
+echo "Build successful"
+grub-mkrescue --output=xfxos.iso
